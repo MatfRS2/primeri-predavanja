338af9b143085d17f3dc171c3a5f755fcaec6040
Vladimir Filipovic <vladofilipovic@hotmail.com>
2024-12-05 08:39:40 +0100

    popravio

---
+++ 01-language/01-functions/01-simple-case/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Text;
+
+namespace RS2.Funkcije.Jednostavno
+{
+    class Program
+    {
+        static void PrikazDo42()
+        {
+            int i = 40;
+            while (i <= 42)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        static void StampajViseLinija(int z)
+        {
+            for (int i = 0; i < z; i++)
+                Console.WriteLine("-- " + i + " --");
+        }
+
+        static int BrojSlova(string s)
+        {
+            int ret = 0;
+            foreach (char ch in s)
+            {
+                if (ch >= 'a' && ch <= 'z')
+                    ret++;
+                if (ch >= 'A' && ch <= 'Z')
+                    ret++;
+            }
+            return ret;
+        }
+
+        static string Nalepi(string s, int brojPonavljanja)
+        {
+            if (brojPonavljanja <= 1)
+                return s;
+            StringBuilder graditelj = new StringBuilder(s);
+            for (int i = 1; i < brojPonavljanja; i++)
+            {
+                graditelj.Append(s);
+            }
+            return graditelj.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            PrikazDo42();
+            Console.WriteLine();
+
+            int z = 2;
+            StampajViseLinija(z);
+            Console.WriteLine("Zdravo svete!");
+            StampajViseLinija(z);
+            Console.WriteLine();
+
+            z = BrojSlova("123 Miki Maus 123");
+            Console.WriteLine(z);
+            Console.WriteLine(Nalepi("Miki Maus ", 2));
+            Console.WriteLine(Nalepi(brojPonavljanja: 4, s: "Paja Patak "));
+            Console.WriteLine();
+        }
+    }
+}
+
+/* Izlaz dobijen prilikom izvrsavanja programa:
+40
+41
+42
+
+-- 0 --
+-- 1 --
+Zdravo svete!
+-- 0 --
+-- 1 --
+
+8
+Miki Maus Miki Maus
+Paja Patak Paja Patak Paja Patak Paja Patak
+
+*/
+
---
+++ 01-language/01-functions/03-ref-params/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+using System;
+
+namespace RS2.Funkcije.ParametriRef;
+
+class Program
+{
+
+    // Vrši se supstitucija po vrednosti
+    static void DodajVrednost(int x)
+    {
+        x += 10;
+    }
+
+    // Vrši se supstitucija po referenci
+    static void OduzmiVrednost(ref int y)
+    {
+        y -= 5;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // Inicijalizuj vrednosti za a, b 
+        int a = 10, b=10;
+
+        // Prikaži inicijalne vrednosti
+        Console.WriteLine("Vrednost promenljive {0} je {1}", nameof(a), a);
+        Console.WriteLine("Vrednost promenljive {0} je {1}", nameof(b), b);
+        Console.WriteLine();
+
+        DodajVrednost(a);
+        Console.WriteLine("Vrednost promenljive {0} " +
+            "nakon izvršenog dodavanja je {1}", nameof(a), a);
+        Console.WriteLine();
+
+        OduzmiVrednost(ref b);
+        Console.WriteLine("Vrednost promenljive {0} " +
+            "nakon izvršenog oduzimanja je {1}", nameof(b), b);
+        Console.WriteLine();
+    }
+
+}
+
+/* Izlaz dobijen prilikom izvršavanja programa:
+Vrednost promenljive a je 10
+Vrednost promenljive b je 12
+
+Vrednost promenljive a nakon izvršenog dodavanja je 10
+
+Vrednost promenljive b nakon izvršenog oduzimanja je 7
+
+*/
